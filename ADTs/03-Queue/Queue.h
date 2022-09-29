615d9492790fd92a2bc1b4a901f2ab560de4d0e1
Gabriel Roque <56568228+Gabito264@users.noreply.github.com>
2022-09-29 16:45:56 -0500

    Inicio Queue

--- ADTs/03-Queue/Queue.h
+++ ADTs/03-Queue/Queue.h
@@ -14,11 +14,23 @@ public:
     T pop();
     T getFront();
     void print();
+    bool isEmpty();
 };
 
 template <class T>
 Queue<T>::Queue()
 {
+    front = NULL;
+    size = 0;
 }
 
+
+
+template<class T>
+bool Queue<T>:: isEmpty(){
+    if(front == NULL){
+        return true;
+    }
+    return false
+}
 #endif
---
+++ ADTs/03-Queue/QueueTest.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "Queue.h"
+
+using namespace std;
+int main()
+{
+    
+    
+    
+    return 0;
+}
