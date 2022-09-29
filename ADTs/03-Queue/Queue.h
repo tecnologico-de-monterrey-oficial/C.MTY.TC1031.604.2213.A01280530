bbbf616e135e9fca5c31053d605b0073c63d933f
Gabriel Roque <56568228+Gabito264@users.noreply.github.com>
2022-09-29 17:21:53 -0500

    Terminar Queue

--- ADTs/03-Queue/Queue.h
+++ ADTs/03-Queue/Queue.h
@@ -23,14 +23,72 @@ Queue<T>::Queue()
     front = NULL;
     size = 0;
 }
+template <class T>
+void Queue<T>::push(T data)
+{
+        if (isEmpty())
+        {
+            //if it's empty, we add the front
+            front = new Node<T>(data);
+            size++;
+        }else{
+            Node<T>* aux = front;
+            while(aux->next != NULL){
+                aux = aux->next;
+            }
+            aux->next= new Node<T>(data);
+            size++;
+        }
+    
+}
 
+template<class T>
+T Queue<T>::pop(){
+    //Remove the first element and return data
+    if (isEmpty()==false)
+    {
+        Node<T>* destroy = front;
+        front = front->next;
+        T data = destroy->data;
+        delete destroy;
+        return data;
+    }else{
+        throw out_of_range("Lista vacía");
+    }
+    
+}
 
+template<class T>
+T Queue<T>::getFront(){
+    if (isEmpty() == false)
+    {
+        return front->data;
+    }else{
+        throw out_of_range("Lista vacía");
+    }
+}
+
+template<class T>
+void Queue<T>::print(){
+    if (isEmpty() == false)
+    {
+        Node<T>* aux = front;
+        while (aux != NULL)
+        {
+            cout<< aux->data <<" ";
+            aux = aux->next;
+        }
+        cout<<" "<<endl;
+    }else{
+        cout<<"La lista está vacía"<<endl;
+    }
+}
 
 template<class T>
 bool Queue<T>:: isEmpty(){
     if(front == NULL){
         return true;
     }
-    return false
+    return false;
 }
 #endif
--- ADTs/03-Queue/QueueTest.cpp
+++ ADTs/03-Queue/QueueTest.cpp
@@ -1,11 +1,19 @@
 #include <iostream>
+using namespace std;
 #include "Queue.h"
 
-using namespace std;
+
 int main()
 {
-    
-    
-    
+    Queue<string> x =  Queue<string>();
+    x.push("hola");
+    x.push("crayola");
+    x.push("como te va");
+    x.print();
+
+    x.pop();
+    x.print();
+    cout<<x.getFront()<<endl;
+    x.print();
     return 0;
 }
